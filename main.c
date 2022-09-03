9f073b6369f4289c9e929baadebc1b6dfbd9aa97
rajpronit <pronitraj.02gmail.com>
2022-09-04 03:18:14 +0530

    taking input from the keyboard

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {   
-    printf("Hello!");
+    printf("Hello!"); 
+    int n;
+    printf("Enter the value of n:- ");
+    scanf("%d",&n);
+    printf("The square of %d is %d ",n,n*n);
     return 0;
 }
